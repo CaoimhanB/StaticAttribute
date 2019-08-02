4a9190a627efbbd2b0e2934a8155c838c85c9336
Caoimhan Bradley <caoimhan@alchemytechs.com>
2019-08-02 10:55:48 +0100

    Add project files.

---
+++ StaticAttribute/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Song holiday = new Song("Holiday", "Green Day", 200);
+            Console.WriteLine(Song.songCount);
+
+            Song kashmir = new Song("Kashmir", "Led Zeppelin", 150);
+
+            Console.WriteLine(kashmir.getSongCount());
+
+            Console.ReadLine();
+        }
+    }
+}
